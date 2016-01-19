875719cb5d0d4857ae0b5c773b325211ca8a4698
Michal Turek <turek@avast.com>
2016-01-19 10:25:19 +0100

    Automatized deployment to servers.

---
+++ deploy_to_servers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVERS='kafka01-test kafka02-test kafka03-test'
+REMOTE_DIR='kafka-tests_new'
+
+for server in ${SERVERS}
+do
+    echo "==================== ${server} ===================="
+    rsync -av *.sh "${server}:${REMOTE_DIR}/"
+    rsync -av target/*.jar "${server}:${REMOTE_DIR}/target/"
+    rsync -av config_servers/* "${server}:${REMOTE_DIR}/config/"
+    echo
+done
